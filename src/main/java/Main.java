235bc681840d691bca0249fbd22f5c8b1653c70c
trweiss17 <142759304+trweiss17@users.noreply.github.com>
2024-02-02 19:49:53 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,47 @@ public class Main {
 		PalindromeChecker sim = new PalindromeChecker();
         	sim.tester();
 	}
-	
+	public void setup()
+{
+  String lines[] = {"test","rotator","rewriter","nurses run", "Madam, I'm Adam!","A Man! A Plan! A Canal! Panama!"};
+  System.out.println("there are " + lines.length + " lines");
+  for (int i=0; i < lines.length; i++) 
+  {
+    if(palindrome(lines[i])==true)
+    {
+      System.out.println(lines[i] + " IS a palindrome.");
+    }
+    else
+    {
+      System.out.println(lines[i] + " is NOT a palindrome.");
+    }
+  }
+}
+public boolean palindrome(String word)
+{
+  String lWord= new String();
+  
+  for(int i=0; i<word.length(); i++){
+    if(Character.isLetter(word.charAt(i))){
+    lWord= lWord + word.charAt(i);
+    }
+  }
+  lWord = lWord.toLowerCase();
+  
+  if(reverse(lWord).equals(lWord)){
+    return true;
+  }
+  return false;
+ }
+
+public String reverse(String str)
+{
+    String sNew = new String();
+    for(int i= str.length(); i>0; i--){
+      sNew = sNew + str.substring(i-1, i);
+    }
+    return sNew;
+}
 
 }
 
